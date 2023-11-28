d2742035aa0417971abe5523273b25aca63bd8f2
millatur <minnatur7373@gmail.com>
2023-11-29 05:56:32 +0600

    today my first commit and first

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return '<h1>Hello World</h1>';
+});
+
+
+Route::get('/feed', function () {
+    return '<h1>Hello World</h1>';
+});
+
+
+Route::get('/profile', function () {
+    return '<h1>Hello World</h1>';
+});
+
