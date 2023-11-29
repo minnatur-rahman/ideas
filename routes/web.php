96e74634eb3af2f2896bd45fc3366a44f108e115
millatur <minnatur7373@gmail.com>
2023-11-29 06:30:24 +0600

    today is my second commit

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return '<h1>Hello World</h1>';
+    return view('welcome');
 });
 
 
 Route::get('/feed', function () {
-    return '<h1>Hello World</h1>';
+    return view('feed');
 });
 
 
 Route::get('/profile', function () {
-    return '<h1>Hello World</h1>';
+    return view('users.profile');
 });
 
