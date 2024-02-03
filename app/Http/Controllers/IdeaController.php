6e02eef12433058bfef4c371bc90821ad7a3afc9
millatur <minnatur7373@gmail.com>
2024-02-04 00:11:41 +0600

    edit some data

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -12,6 +12,6 @@ class IdeaController extends Controller
         $idea = Idea::create([
             'content'=> request()->get('idea',''),
         ]);
-        return redirect()->route('dashboard');
+        return redirect()->route('dashboard')->with('success', 'Idea created successfully!!');
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -37,10 +37,7 @@
                 </div>
             </div>
             <div class="col-6">
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    Idea created Successfully
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
+                @include('shared.success-message')
                 @include('shared.submit-idea')
                 <hr>
                 @foreach ($ideas as $idea)
