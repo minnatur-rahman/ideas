bb8259144f4e63f2f57e2bc3e0aecc69ba3a9fef
millatur <minnatur7373@gmail.com>
2024-02-04 23:39:16 +0600

    destroy function is ok

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -18,4 +18,11 @@ class IdeaController extends Controller
         ]);
         return redirect()->route('dashboard')->with('success', 'Idea created successfully!!');
     }
+
+    public function destroy($id){
+
+        $idea = Idea::where('id',$id)->firstOrFail()->delete();
+
+         return redirect()->route('dashboard')->with('success', 'Idea deleted successfully!!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::post('/idea', [IdeaController::class, 'store'])->name('idea.create');
+Route::post('/ideas', [IdeaController::class, 'store'])->name('idea.create');
+
+Route::delete('/ideas/{id}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
 
 Route::get('/terms', function(){
     return view('terms');
