34e3ba24627f1972430762f57a03d2be776b9b24
millatur <minnatur7373@gmail.com>
2024-02-06 22:03:30 +0600

    edit searchbar

--- resources/views/shared/search-bar.blade.php
+++ resources/views/shared/search-bar.blade.php
@@ -4,9 +4,9 @@
     </div>
     <div class="card-body">
         <form action="{{ route('dashboard') }}" method="GET">
-        <input name="search" placeholder="...
-        " class="form-control w-100" type="text">
-        <button class="btn btn-dark mt-2"> Search</button>
+           <input name="search" placeholder="...
+                " class="form-control w-100" type="text">
+           <button class="btn btn-dark mt-2"> Search</button>
         </form>
     </div>
 </div>
